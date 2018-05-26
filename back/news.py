12f279f1eadbc395c047ade649361d8599017c4a
Raymond J. Wu <wu.raymond.j@gmail.com>
2018-05-26 12:50:53 -0400

    comment out debug lines

--- back/news.py
+++ back/news.py
@@ -1,13 +1,4 @@
-#!/usr/bin/python
-# -*- coding: UTF-8 -*-
-
-import cgi
-import cgitb:
 import requests
-cgitb.enable()
-
-print "content-Type: text/html\n"
-
 
 def getURL(endpoint, query):
     retURL = "https://newsapi.org/v2/"
@@ -25,7 +16,9 @@ def getURL(endpoint, query):
     return retURL + "&apiKey=a842f08935ec4c4f8cbfa0ca729fc2c1"
 
 
-print ( getURL("", "barack obama") )
-response = requests.get(getURL("","barack obama"))
+## for debugging purposes...
 
-print(response.json())
+# print ( getURL("", "barack obama") )
+
+# response = requests.get(getURL("","barack obama"))
+# print ( response.json() )
