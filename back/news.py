b4eea8ef5fca941e20baf453db7a825a42933824
Raymond J. Wu <wu.raymond.j@gmail.com>
2018-05-26 12:10:35 -0400

    remove unnecessary methods

--- back/news.py
+++ back/news.py
@@ -1,8 +1,6 @@
-from bs4 import BeautifulSoup
 from newsapi import NewsApiClient
-import requests
 
-newsapi = NewsApiClient(api_key='a842f08935ec4c4f8cbfa0ca729fc2c1')
+client = api.NewsApiClient(api_key='a842f08935ec4c4f8cbfa0ca729fc2c1')
 
 def get_news(search):
     news = newsapi.get_top_headlines(q=search, language="en")
