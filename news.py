d58205d62ac683c4589ef18b6473f13ac5995f8c
Raymond J. Wu <wu.raymond.j@gmail.com>
2018-05-26 17:04:14 -0400

    fix ascii

--- news.py
+++ news.py
@@ -347,7 +347,7 @@ def main():
         except:
             break
     
-    return_string = return_string.replace(u"\u2018", "'").replace(u"\u2019", "'").replace(r"\uxe9", "'")
+    return_string = return_string.replace(u"\u2018", "'").replace(u"\u2019", "'").encode('ascii', 'ignore')
     return HEADER + return_string + FOOTER
 
 print(main())
