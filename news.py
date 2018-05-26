d7bb210a7e67a2a33eb9965be6f038d1359e44f0
Raymond J. Wu <wu.raymond.j@gmail.com>
2018-05-26 13:46:41 -0400

    clean up file

--- news.py
+++ news.py
@@ -77,38 +77,3 @@ def sourceData(endpoint, query):
     url = getURL(endpoint, query)
     response = requests.get(url)
     return response.json()
-
-
-## debugging ...
-
-
-## testing getURL ##
-
-
-# print ( getURL("", "barack obama") )
-# response = requests.get(getURL("","barack obama"))  # should return everything abt B. Obama
-# print ( response.json() )
-
-# print ( "#" * 50 )
-
-# print ( getURL("top-headlines", "") )
-# response = requests.get(getURL("everything",""))    # should return top headlines abt NYC
-# print ( response.json() )
-
-# print ( "#" * 50 )
-
-# print ( getURL("", "") )
-# response = requests.get(getURL("everything",""))    # should return everything abt NYC
-# print ( response.json() )
-
-
-
-## testing getHeadlines
-
-
-# print ( sourceData("", "barack obama" ) )  # should return everything abt B. Obama
-# print ( "#" * 50 )
-# print ( sourceData("top-headlines", "") )  # should return top headlines abt NYC
-# print ( "#" * 50 )
-# print ( sourceData("everything", "")    )  # should return everything abt NYC
-
