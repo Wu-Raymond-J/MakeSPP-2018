85d47e2b98863f36c8868d6812e628863bb53a0b
Raymond J. Wu <wu.raymond.j@gmail.com>
2018-05-26 12:22:11 -0400

    Use requests instead of newsapi module

--- back/news.py
+++ back/news.py
@@ -1,9 +1,9 @@
-from newsapi import NewsApiClient
+import requests
 
-newsapi = NewsApiClient(api_key='a842f08935ec4c4f8cbfa0ca729fc2c1')
+url = ("https://newsapi.org/v2/top-headlines?"
+       "country=us&"
+       "apiKey=a842f08935ec4c4f8cbfa0ca729fc2c1")
 
-def get_news(search):
-    news = newsapi.get_top_headlines(q=search, language="en")
-    print(news)
+response = requests.get(url)
 
-get_news("test")
+print ( response.json() )
