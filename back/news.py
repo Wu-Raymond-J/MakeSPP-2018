a3f414a53c9f06f866dd185fa10aaecd09407b73
Raymond J. Wu <wu.raymond.j@gmail.com>
2018-05-26 12:12:41 -0400

    debug

--- back/news.py
+++ back/news.py
@@ -1,6 +1,6 @@
 from newsapi import NewsApiClient
 
-newsapi = api.NewsApiClient(api_key='a842f08935ec4c4f8cbfa0ca729fc2c1')
+newsapi = NewsApiClient(api_key='a842f08935ec4c4f8cbfa0ca729fc2c1')
 
 def get_news(search):
     news = newsapi.get_top_headlines(q=search, language="en")
