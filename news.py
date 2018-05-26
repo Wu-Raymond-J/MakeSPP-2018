b7d144a8b4f9727cf60ff8a702a37ddf0fee81d7
Rubin Peci <rubinpeci@gmail.com>
2018-05-26 17:01:24 -0400

    n-ahwoqebqwe

--- news.py
+++ news.py
@@ -257,7 +257,6 @@ def parseFormData():
     # endpoint = form.getValue('endpoint')
     query = form.getvalue('search')
     #### TEMPORARY
-    query = "new york city"
     json = sourceData(endpoint, query)
     return json
 
