149b1e4968afd15ae98af321699c94f357d31535
Raymond J. Wu <wu.raymond.j@gmail.com>
2018-05-26 12:46:28 -0400

    add getURL method

--- back/news.py
+++ back/news.py
@@ -1,9 +1,22 @@
 import requests
 
-url = ("https://newsapi.org/v2/top-headlines?"
-       "country=us&"
-       "apiKey=a842f08935ec4c4f8cbfa0ca729fc2c1")
+def getURL(endpoint, query):
+    retURL = "https://newsapi.org/v2/"
 
-response = requests.get(url)
+    if endpoint != "":
+        # endpoint in ["top-headlines", "everything"]
+        retURL += endpoint + "?"
+    else:
+        # default = everything
+        retURL += "everything?"
+
+    if query != "":
+        retURL += "q=" + query.replace(" ", "+")
+
+    return retURL + "&apiKey=a842f08935ec4c4f8cbfa0ca729fc2c1"
+
+
+print ( getURL("", "barack obama") )
+response = requests.get(getURL("","barack obama"))
 
 print ( response.json() )
