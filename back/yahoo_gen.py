773a0c221029a0e4c92ac8578e7736ddb479e58e
Rubin Peci <rubinpeci@gmail.com>
2018-05-26 11:17:42 -0400

    Split up urlGen into one for Google and Yahoo

---
+++ back/yahoo_gen.py
@@ -0,0 +1,8 @@
+from bs4 import BeautifulSoup
+import urllib
+
+def url_gen(str):
+    return "https://www.google.com/search?q=" + str.replace(" ", "+") + "&tbm=nws"
+
+print(url_gen("game"))
+print(url_gen("political news"))
