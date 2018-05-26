ff656a2c1dfc1ce749ffad00ab9c9a24d0cbda4c
Rubin Peci <rubinpeci@gmail.com>
2018-05-26 11:58:33 -0400

    Implement newsapi

--- back/google_gen.py
+++
@@ -1,8 +0,0 @@
-from bs4 import BeautifulSoup
-import urllib
-
-def url_gen(str):
-    return "https://www.google.com/search?q=" + str.replace(" ", "+") + "&tbm=nws"
-
-print(url_gen("game"))
-print(url_gen("political news"))
