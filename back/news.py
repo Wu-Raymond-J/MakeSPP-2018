054eb3e0cbfa274b4b7c8cc83867875b85c11120
Rubin Peci <rubinpeci@gmail.com>
2018-05-26 12:07:09 -0400

    Remove unecessary methods

--- back/news.py
+++ back/news.py
@@ -4,14 +4,6 @@ import requests
 
 newsapi = NewsApiClient(api_key='a842f08935ec4c4f8cbfa0ca729fc2c1')
 
-def url_gen(str):
-    return "https://www.google.com/search?q=" + str.replace(" ", "+") + "&tbm=nws"
-
-def page_scraper(url):
-    page = urllib.request.urlopen(url)
-    content = BeautifulSoup(page, "html.parser")
-    print(content)    
-
 def get_news(search):
     news = newsapi.get_top_headlines(q=search, language="en")
     print(news)
