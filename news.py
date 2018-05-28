9f544993005cec8d8c7128226fbf2a0b12374ef1
Rubin Peci <rubinpeci@gmail.com>
2018-05-27 22:45:43 -0400

    Fix errors when some info was missing from the api

--- news.py
+++ news.py
@@ -288,19 +288,19 @@ def parse_dictionary():
     else:
         articles = dictionary["articles"]
         for article in articles:
-            if 'title' in article:
+            if 'title' in article and article['title'] is not None:
                 list_of_titles.append(article["title"].encode("utf-8"))
 
-            if 'author' in article:
+            if 'author' in article and article['author'] is not None:
                 list_of_authors.append(article["author"].encode("utf-8"))
 
-            if 'source' in article:
+            if 'source' in article and article['source'] is not None:
                 list_of_sources.append(article['source']['name'].encode("utf-8"))
 
-            if 'url' in article:
+            if 'url' in article and article['url'] is not None:
                 list_of_urls.append(article['url'].encode("utf-8"))
 
-            if 'urlToImage' in article:
+            if 'urlToImage' in article and article['urlToImage'] is not None:
                 list_of_images.append(article['urlToImage'].encode("utf-8"))
 
         return_dictionary["titles"] = list_of_titles
